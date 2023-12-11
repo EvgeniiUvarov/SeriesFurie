2be6662b0d22b5d0a6e64533434c653f9f669496
Evgenii Uvarov <ev9en1149@gmail.com>
2023-12-11 09:04:58 +0300

    FizBuzz

--- Program.cs
+++ Program.cs
@@ -2,3 +2,12 @@
 using static System.Console;
 Clear();
 
+string cout = string.Empty;
+
+  for(int i=1;i<101;i++)
+  {
+      cout += i%3==0?$" Fiz({i})": i%5==0?$" Buzz({i})":" ";
+      if(i%3==0 && i%5==0) cout +=$" Eys({i})";
+  }
+
+WriteLine(cout);
