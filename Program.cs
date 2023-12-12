71fc65e8071a594a9b368c50bd989f8c74c1b461
Evgenii Uvarov <ev9en1149@gmail.com>
2023-12-12 10:38:26 +0300

    end number

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 using static System.Console;
 Clear();
 
-string cout = string.Empty;
+WriteLine("Write number: ");
+int numA = int.Parse(ReadLine());
 
-  for(int i=1;i<101;i++)
-  {
-      cout += i%3==0?$" Fiz({i})": i%5==0?$" Buzz({i})":" ";
-      if(i%3==0 && i%5==0) cout +=$" Eys({i})";
-  }
+if (Math.Abs(numA) < 99)
+{
+   WriteLine("Введено не трёх значное число: ");
+   return;
+}
+WriteLine(numA%100%10);
 
-WriteLine(cout);
+
+
