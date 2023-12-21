74a03f9e09793fb2bd82bd906100863c4cca66ff
Evgenii Uvarov <ev9en1149@gmail.com>
2023-12-21 09:30:58 +0300

    Palindrom

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+using System;
 using static System.Console;
 Clear();
 
-WriteLine("Write number: ");
-int numA = int.Parse(ReadLine());
+WriteLine("Введите пятизначное число: ");
+string num = ReadLine();
 
-if (Math.Abs(numA) < 99)
-{
-   WriteLine("Введено двух значное число: ");
-   return;
-}
-WriteLine(numA%100%10);
+string res = num.Length < 5 || num.Length > 5?"Введено не пятизначное число!"
+                                             :num[0] == num[4] && num[1] == num[3]?"Число является палиндромом!"
+                                             :"Число не является палиндромом";
 
-
-
+WriteLine(res);
