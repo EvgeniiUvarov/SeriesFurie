e61db3492a470e34a6be0b983e4c49a2f5327b3f
Evgenii Uvarov <ev9en1149@gmail.com>
2023-12-28 21:21:30 +0300

    new commit

--- Program.cs
+++ Program.cs
@@ -1,21 +1,53 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
+﻿// **Задача 32:** Напишите программу замена элементов массива: положительные элементы замените на соответствующие 
+//   отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 
 using System;
 using static System.Console;
 Clear();
 
-Write("Введите числа: ");
-WriteLine(String.Join(" ", RandArray(int.Parse(ReadLine()))));
+Write("Введите размер массива: ");
+int siz = int.Parse(ReadLine());
+Write("Введите искомое число: ");
+int num = int.Parse(ReadLine());
+int[] array = RandArray(siz);
+WriteLine($"{String.Join(" ,", array)}");
+WriteLine($"{string.Join(" ",SumPozNum(array))}");
+WriteLine(FindNum(array,num));
+
+
+int[] SumPozNum(int[] array)
+{
+   int[] result = new int[array.Length];
+   for (int i = 0; i < array.Length; i++)
+   {
+      result[i] = array[i]*-1;
+   }
+   return result;
+}
+
+string FindNum(int[] array, int num)
+{
+   string res = string.Empty;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] == num)
+      {
+       res = "eys";
+       break;
+      }
+      else res = "no";
+   }
+   return res;
+}
 
 int[] RandArray(int size)
 {
    int[] result = new int[size];
    for (int i = 0; i < size; i++)
    {
-      result[i] = new Random().Next(1,10);
+      result[i] = new Random().Next(-9,10);
    }
    return result;
 }
