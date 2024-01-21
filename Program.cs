c8e931c3761c548438bd4ae319fd0cf2341a5f56
Evgenii Uvarov <ev9en1149@gmail.com>
2024-01-21 11:48:01 +0300

    funktion

--- Program.cs
+++ Program.cs
@@ -1,21 +1,17 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 
 using static System.Console;
 Clear();
 
-Write("Введите числа через пробел: ");
-string[] strArr = ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+Write("Введите числа через пробел K1 B1 K2 B2: ");
+string[] stArr = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+double k1 = int.Parse(stArr[0]);
+double b1 = int.Parse(stArr[1]);
+double k2 = int.Parse(stArr[2]);
+double b2 = int.Parse(stArr[3]);
 
-WriteLine($"{SumArray(strArr)}");
-
-int SumArray(string[] stringAr)
-{
-   int result = 0;
-   for (int i = 0; i < stringAr.Length; i++)
-   {
-      result += int.Parse(stringAr[i]) > 0? 1:0;
-   }
-   return result;
-}
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+WriteLine($"Точка пересечения двух прямых: ({x} {y})");
