c370438167f64465e90dc26eaf7dc229a781ed54
Evgenii Uvarov <ev9en1149@gmail.com>
2024-01-25 12:12:38 +0300

    Doubl Array

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿// Задача 49:** Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
-// Например, изначально массив выглядел вот так:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
 
 using static System.Console;
 Clear();
@@ -12,45 +12,30 @@ string[] strAr = ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
 int rows = int.Parse(strAr[0]);
 int colum = int.Parse(strAr[1]);
 
-int[,] arr = GetArray(rows,colum);
+double[,] arr = GetArray(rows,colum);
 PrintArray(arr);
-ChotIndexArray(arr);
-WriteLine();
-PrintArray(arr);
+WriteLine(arr);
 
-void ChotIndexArray(int[,] array)
+double[,] GetArray(int row, int col)
 {
-   for (int i = 0; i < array.GetLength(0); i++)
-   {
-      for (int j = 0; j < array.GetLength(1); j++)
-      {
-         array[i,j] = i %2 == 0 && j %2 == 0?array[i,j] *= array[i,j]:array[i,j];
-         //if( i %2 == 0 & j %2 == 0) array[i,j] *= array[i,j];
-         //if( i %2 == 0 & j %2 == 0) array[i,j] = Convert.ToInt32(Math.Pow(array[i,j],2));
-      }
-   }
-}
-
-int[,] GetArray(int row, int col)
-{
-   int[,] result = new int[row,col];
+   double[,] result = new double[row,col];
    for (int i = 0; i < result.GetLength(0); i++)
    {
       for (int j = 0; j < result.GetLength(1); j++)
       {
-         result[i,j] = new Random().Next(1,6);
+         result[i,j] = Convert.ToDouble(new Random().Next(-9,10));
       }
    }
    return result;
 }
 
-void PrintArray(int[,] array)
+void PrintArray(double[,] array)
 {
    for (int i = 0; i < array.GetLength(0); i++)
    {
       for (int j = 0; j < array.GetLength(1); j++)
       {
-         Write($"{array[i,j]} ");
+         Write($"{array[i,j]:f1} ");
       }
       WriteLine();
    }
