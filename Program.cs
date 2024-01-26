606643f4cd57be1628afd1ba5f9b67efb8abad32
Evgenii Uvarov <ev9en1149@gmail.com>
2024-01-26 11:17:39 +0300

    Serch Pos

--- Program.cs
+++ Program.cs
@@ -1,41 +1,64 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
 
+using System.Diagnostics;
 using static System.Console;
 Clear();
 
 Write("Введите через пробел размер двумерного массива: ");
-string[] strAr = ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+string[] strAr = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 int rows = int.Parse(strAr[0]);
 int colum = int.Parse(strAr[1]);
+int[,] array = GetArray(rows, colum);
+PrintArray(array);
 
-double[,] arr = GetArray(rows,colum);
-PrintArray(arr);
-WriteLine(arr);
+WriteLine("Введите позиции элемента: ");
+string[] positionEll = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int rowPos = int.Parse(positionEll[0]);
+int colPos = int.Parse(positionEll[1]);
+//WriteLine($"{SerchPositionElement(array,rowPos,colPos)}");
 
-double[,] GetArray(int row, int col)
+if(array.GetLength(0) < rowPos && array.GetLength(1) < colPos) WriteLine("Такого элемента нет");
+else WriteLine($"Элемент: {array[rowPos,colPos]}");
+
+// string SerchPositionElement(int[,] array, int row, int col)
+// {
+//    string result = "Такого элемента нет!";
+//    for (int i = 0; i < array.GetLength(0); i++)
+//    {
+//       for (int j = 0; j < array.GetLength(1); j++)
+//       {
+//          if(i == row && j == col) result = $"Элемент найден {array[i,j]}";
+//       }
+//    }
+//    return result;
+// }
+
+int[,] GetArray(int row, int col)
 {
-   double[,] result = new double[row,col];
+   int[,] result = new int[row, col];
    for (int i = 0; i < result.GetLength(0); i++)
    {
       for (int j = 0; j < result.GetLength(1); j++)
       {
-         result[i,j] = Convert.ToDouble(new Random().Next(-9,10));
+         result[i, j] = new Random().Next(1, 10);
       }
    }
    return result;
 }
 
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
    for (int i = 0; i < array.GetLength(0); i++)
    {
       for (int j = 0; j < array.GetLength(1); j++)
       {
-         Write($"{array[i,j]:f1} ");
+         Write($"{array[i, j]} ");
       }
       WriteLine();
    }
