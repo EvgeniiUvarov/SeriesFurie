d87ebc0df4d599d79efac9b6c713ead7f77f2edc
Evgenii Uvarov <ev9en1149@gmail.com>
2024-02-07 10:49:49 +0300

    Checnch Rows end Column

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+﻿//Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
 
 using System.Diagnostics;
 using static System.Console;
@@ -13,25 +8,33 @@ Write("Введите через пробел размер двумерного 
 string[] strAr = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 int rows = int.Parse(strAr[0]);
 int colum = int.Parse(strAr[1]);
-int[,] array = GetArray(rows, colum);
+int[,] array = GetArray(rows,colum);
+PrintArray(array);
+ChenchRowCol(array);
+WriteLine();
+PrintArray(array);
+WriteLine();
+ChenchColRow(array);
 PrintArray(array);
 
-WriteLine($"{String.Join(' ',SerchPositionElement(array))}");
-
-double[] SerchPositionElement(int[,] array)
+void ChenchRowCol(int[,] array)
 {
-   double[] result = new double[array.GetLength(1)];
-
    for (int i = 0; i < array.GetLength(1); i++)
    {
-      double sum = 0;
-      for (int j = 0; j < array.GetLength(0); j++)
-      {
-         sum += array[j,i];
-      }
-      result[i] = sum/array.GetLength(0);
+         int res = array[array.GetLength(0)-1,i];
+         array[array.GetLength(0)-1,i] = array[0,i];
+         array[0,i] = res;
    }
-   return result;
+}
+
+void ChenchColRow(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      int res = array[i,array.GetLength(1)-1];
+      array[i,array.GetLength(1)-1] = array[i,0];
+      array[i,0] = res;
+   }
 }
 
 int[,] GetArray(int row, int col)
