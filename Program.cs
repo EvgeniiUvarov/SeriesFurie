944ddb9ff5d5410cd16e86a637d8b335131caba1
Evgenii Uvarov <ev9en1149@gmail.com>
2024-02-08 11:18:13 +0300

    Alphavit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
-// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, 
+// сколько раз встречается элемент входных данных.
+// Если набор данных — таблица:
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
 
-using System.Diagnostics;
 using static System.Console;
 Clear();
 
@@ -9,29 +17,53 @@ Write("Введите через пробел размер двумерного 
 string[] strAr = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 int rows = int.Parse(strAr[0]);
 int colum = int.Parse(strAr[1]);
+
 int[,] array = GetArray(rows, colum);
+PrintArray(array);
+WriteLine();
 
-if (rows == colum)
+for (int i = MinElement(array); i < MaxElement(array)+1; i++)
 {
-   PrintArray(array);
-   ChenchRowsColumns(array);
-   WriteLine();
-   PrintArray(array);
+   if(SerchSumElement(array,i) != 0) WriteLine($"{i} Встречается {SerchSumElement(array,i)} раз!");
 }
-else WriteLine("Not possible");
 
-void ChenchRowsColumns(int[,] array)
+int SerchSumElement(int[,] array, int num)
 {
+   int sum = 0;
    for (int i = 0; i < array.GetLength(0); i++)
    {
-      for (int j = i; j < array.GetLength(1); j++)
+      for (int j = 0; j < array.GetLength(1); j++)
       {
-         int res = array[i, j];
-         array[i, j] = array[j, i];
-         array[j, i] = res;
+         if (num == array[i,j]) sum++; 
       }
    }
+   return sum;
+}
 
+int MinElement(int[,] array)
+{
+   int result = array[0,0];
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 1; j < array.GetLength(1); j++)
+      {
+         if (array[i,j] < result) result = array[i,j];
+      }
+   }
+   return result;
+}
+
+int MaxElement(int[,] array)
+{
+   int result = array[0,0];
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+      for (int j = 1; j < array.GetLength(1); j++)
+      {
+         if (array[i,j] > result) result = array[i,j];
+      }
+   }
+   return result;
 }
 
 int[,] GetArray(int row, int col)
