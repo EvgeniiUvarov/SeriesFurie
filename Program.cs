72562348bbaa6b134d4b99096e5cc70abfa6bf80
Evgenii Uvarov <ev9en1149@gmail.com>
2024-03-17 10:17:16 +0300

    Ackerman

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
-﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
 using static System.Console;
 Clear();
 
-Write("Введите два числа через пробел: ");
+WriteLine("Введите числа m и n через пробел: ");
 string[] str = ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
 int numA = int.Parse(str[0]);
 int numB = int.Parse(str[1]);
 
-WriteLine(RecursMethod(numA,numB));
+WriteLine(RecursMethod(numB,numA));
 
 int RecursMethod(int numA, int numB)
 {
-   return numA <= numB ?numA + RecursMethod(numA+1, numB): 0;
+   return numA == 0? numB+1: numB == 0? RecursMethod(numA-1, 1) :RecursMethod(numA-1, RecursMethod(numA, numB-1));
 }
 
